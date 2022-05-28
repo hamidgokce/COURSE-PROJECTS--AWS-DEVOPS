2c1ef7da961259c683bce0e9ea167ebed0599ad8
hamid gokce <hamidgokce@gmail.com>
2022-05-28 22:54:03 +0300

    roman-numerals-converter

--- AWS/Project-102-Roman-Numerals-Converter/app.py
+++ AWS/Project-102-Roman-Numerals-Converter/app.py
@@ -33,5 +33,5 @@ def main_post():
         return render_template('index.html', developer_name='Abdulhamid GOKCE', not_valid=False)
     
 if __name__=='__main__':
-    app.run(debug=True)
-    # app.run(host='0.0.0.0', port=80)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
